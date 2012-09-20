3ce97b6e6b34da60947def7745a884319582dffd
komori <yasushi.komori@gree.co.jp>
2012-09-20 13:23:17 +0900

    add grep

---
+++ grep.pl
@@ -0,0 +1,48 @@
+#! /usr/bin/perl
+
+use strict;
+
+my $file = $ARGV[0];
+my $file2 = $ARGV[1];
+
+open(FH, $file2);
+my @list = <FH>;
+close(FH);
+
+my $user_info;
+foreach my $line( @list ) {
+  my @fact = split('\t' , $line , 2);
+  my $user_id = $fact[0];
+  chomp($user_id);
+  push( @{$user_info->{$user_id}}  , trim($fact[1]) );
+}
+
+
+open(ID , $file);
+my @ids = <ID>;
+close(ID);
+
+foreach my $id( @ids ) {
+  chomp($id);
+  if($user_info->{$id})
+  {
+    foreach my $str(@{$user_info->{$id}}){
+      print $id."\t".$str."\n";
+    }
+  }
+  else
+  {
+      print $id."\tNONE";
+  }
+
+}
+
+sub trim {
+    my @out = @_;
+    for (@out) {
+      s/^\s+//;
+      s/\s+$//;
+    }
+    return wantarray ? @out : $out[0];
+}
+
